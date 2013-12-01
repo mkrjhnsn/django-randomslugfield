dd289c8ad6919b798aebe71ae1dde10922b30e77
melinko <mkrjhnsn@gmail.com>
2013-11-30 23:08:35 -0600

    moved get_runner import.

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,6 @@
 import sys
 
 from django.conf import settings
-from django.test.utils import get_runner
 
 
 def main():
@@ -18,6 +17,7 @@ def main():
         TEMPLATE_DEBUG=True,
     )
 
+    from django.test.utils import get_runner
     test_runner = get_runner(settings)(verbosity=2, interactive=True)
     failures = test_runner.run_tests(['randomslugfield'])
     sys.exit(failures)
