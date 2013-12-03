10ace4fb23785d435b45b46aa728e9edcd82d211
melinko <mkrjhnsn@gmail.com>
2013-12-03 16:46:16 -0600

    change README.rst over to markdown.

--- setup.py
+++ setup.py
@@ -4,15 +4,17 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='django-randomslugfield',
-    version='0.1.1',
-    author='Mike Johnson',
-    author_email='mkrjhnsn@gmail.com',
+    name="django-randomslugfield",
+    version="0.1.1",
+    author="Michael Johnson",
+    author_email="mkrjhnsn@gmail.com",
     packages=['randomslugfield'],
-    url='http://github.com/melinko/django-randomslugfield',
-    license='MIT',
-    description='A Django field that automatically generates random slugs.',
-    long_description=open('README.rst').read(),
-    tests_require=['Django'],
+    url="http://github.com/melinko/django-randomslugfield",
+    license="MIT",
+    description="A Django field that automatically generates random slugs.",
+    long_description="Generates unique random slugs using " \
+        "`'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'` " \
+        "character set.",
+    tests_require=['django'],
     test_suite='run_tests.main',
 )
