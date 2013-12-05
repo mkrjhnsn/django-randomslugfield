0955aff086011d1de46e2f4312c3211f8dde6410
melinko <mkrjhnsn@gmail.com>
2013-12-04 20:23:41 -0600

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,21 @@ setup(
     license="MIT",
     description="A Django field that automatically generates random slugs.",
     long_description="Generates unique random slugs using these characters " \
-        "`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`.",
+        "`abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`. " \
+        "See the project page for more information: " \
+        "http://github.com/melinko/django-randomslugfield",
     tests_require=['django'],
     test_suite='run_tests.main',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+    ],
 )
